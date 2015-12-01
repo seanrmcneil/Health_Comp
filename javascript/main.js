3c992a2deb4d1d6df24cbfcd0b26072c4a8c5bc5
EthanFuld <Ethanfuld58@gmail.com>
2015-12-01 14:07:31 -0500

    got rid of pull-down javascript

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,6 @@
 // 	});
 $(document).ready(function () {
 
-	$('.pull-down').each(function() {
-		$(this).css('margin-top', $(this).parent().height()-$(this).height())
-	});
-
 
 	$('#contactButton').on('click',function(){
             $('html, body').animate({scrollTop: $("#emailForm").offset().top});
