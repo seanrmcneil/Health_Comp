75144f563e59195b8143da6a245e33afe7ff927b
EthanFuld <Ethanfuld58@gmail.com>
2015-12-01 17:17:49 -0500

    KILLIN IT BITCHES

---
+++ budget.php
@@ -0,0 +1,9 @@
+<?php include 'header.php' ?>
+
+<img src="images/budget1.png" alt="budget1" class="budget1">
+
+<a href="budget2.php">
+	<img src="images/requestButton.png" alt="request" class="budgetButton">
+</a>
+
+<?php include 'footer.php' ?>
---
+++ budget2.php
@@ -0,0 +1,6 @@
+<?php include 'header.php' ?>
+
+<img src="images/budget2.png" alt="budget1" class="budget1">
+
+
+<?php include 'footer.php' ?>
